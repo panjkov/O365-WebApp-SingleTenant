69802d5dadec57e0575eaac9407f7d7a5331fee8
panjkov <panjkov@hotmail.com>
2014-12-17 22:06:44 +0100

    NuGet Package Upgrade, MyFiles Controller,View and Model added

---
+++ O365-WebApp-SingleTenant/Controllers/FilesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Office365.Discovery;
+using Microsoft.Office365.OutlookServices;
+using O365_WebApp_SingleTenant.Models;
+using O365_WebApp_SingleTenant.Utils;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.Office365.SharePoint.CoreServices;
+
+namespace O365_WebApp_SingleTenant.Controllers
+{
+    [Authorize]
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public async Task<ActionResult> Index()
+        {
+            List<MyFile> myFiles = new List<MyFile>();
+
+            var signInUserId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var userObjectId = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+
+            AuthenticationContext authContext = new AuthenticationContext(SettingsHelper.Authority, new ADALTokenCache(signInUserId));
+
+            try
+            {
+                DiscoveryClient discClient = new DiscoveryClient(SettingsHelper.DiscoveryServiceEndpointUri,
+                    async () =>
+                    {
+                        var authResult = await authContext.AcquireTokenSilentAsync(SettingsHelper.DiscoveryServiceResourceId, new ClientCredential(SettingsHelper.ClientId, SettingsHelper.AppKey), new UserIdentifier(userObjectId, UserIdentifierType.UniqueId));
+
+                        return authResult.AccessToken;
+                    });
+
+                var dcr = await discClient.DiscoverCapabilityAsync("MyFiles");
+
+                SharePointClient exClient = new SharePointClient(dcr.ServiceEndpointUri,
+                    async () =>
+                    {
+                        var authResult = await authContext.AcquireTokenSilentAsync(dcr.ServiceResourceId, new ClientCredential(SettingsHelper.ClientId, SettingsHelper.AppKey), new UserIdentifier(userObjectId, UserIdentifierType.UniqueId));
+
+                        return authResult.AccessToken;
+                    });
+
+                var filesResult = await exClient.Files.ExecuteAsync();
+
+                do
+                {
+                    var files = filesResult.CurrentPage;
+                    foreach (var file in files)
+                    {
+                        myFiles.Add(new MyFile { Name = file.Name });
+                    }
+
+                    filesResult = await filesResult.GetNextPageAsync();
+
+                } while (filesResult != null);
+            }
+            catch (AdalException exception)
+            {
+                //handle token acquisition failure
+                if (exception.ErrorCode == AdalError.FailedToAcquireTokenSilently)
+                {
+                    authContext.TokenCache.Clear();
+
+                    //handle token acquisition failure
+                }
+            }
+
+            return View(myFiles);
+        }
+    
+    }
+}
---
+++ O365-WebApp-SingleTenant/Models/MyFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace O365_WebApp_SingleTenant.Models
+{
+    public class MyFile
+    {
+
+        public string Name { get; set; }
+    }
+}
